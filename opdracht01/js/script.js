82bbb23c5f97df2578fa6d6eb245c86a5b089ca4
Kimberly Hogeveen <kimberly.hogeveen@gmail.com>
2020-02-06 12:21:58 +0100

    opdracht 1 klaar

--- opdracht01/js/script.js
+++ opdracht01/js/script.js
@@ -6,8 +6,16 @@ window.onload = function () {
     //JOUW CODE HIER
 
     if (go = "groen") {
-        document.getElementById("light-top").innerHTML = go;
-        go.style.color = "green";
+        document.getElementById("light-top").style.backgroundColor = "green";
+
+
+    }
+
+    if (maybe = "oranje") {
+        document.getElementById("light-middle").style.backgroundColor = "orange";
+    }
+
+    if (stop = "red") {
+        document.getElementById("light-bottom").style.backgroundColor = "red";
     }
 }
-
