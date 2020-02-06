32d30a2e9eb927fefda50d6599ec5e8320bac42a
Kimberly Hogeveen <kimberly.hogeveen@gmail.com>
2020-02-06 13:16:36 +0100

    opdracht4 af

--- opdracht04/js/script.js
+++ opdracht04/js/script.js
@@ -2,25 +2,35 @@ var stop = "rood";
 var maybe = "oranje";
 var go = "groen";
 
-window.onload = function(){
+window.onload = function () {
     document.getElementById("light-top").innerHTML = stop;
     document.getElementById("light-middle").innerHTML = maybe;
     document.getElementById("light-bottom").innerHTML = go;
 }
 
-function zetRoodLichtAan(){
-    if(document.getElementById("light-top").style.backgroundColor == "gray"){
+function zetRoodLichtAan() {
+    if (document.getElementById("light-top").style.backgroundColor == "gray") {
         document.getElementById("light-top").style.backgroundColor = "red";
     }
-    else{
+    else {
         document.getElementById("light-top").style.backgroundColor = "gray";
     }
 }
 
-function zetOranjeLichtAan(){
-    document.getElementById("light-middle").style.backgroundColor = "orange";
+function zetOranjeLichtAan() {
+    if (document.getElementById("light-middle").style.backgroundColor = "gray") {
+        document.getElementById("light-middle").style.backgroundColor = "orange";
+    }
+    else {
+        document.getElementById("light-middle").style.backgroundColor = "gray";
+    }
 }
 
-function zetGroenLichtAan(){
-    document.getElementById("light-bottom").style.backgroundColor = "green";
+function zetGroenLichtAan() {
+    if (document.getElementById("light-bottom").style.backgroundColor = "gray") {
+        document.getElementById("light-bottom").style.backgroundColor = "green";
+    }
+    else {
+        document.getElementById("light-bottom").style.backgroundColor = "gray";
+    }
 }
