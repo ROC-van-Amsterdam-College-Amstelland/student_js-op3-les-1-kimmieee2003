a9082e9a68e49a3952ed2553263911561c342313
Kimberly Hogeveen <kimberly.hogeveen@gmail.com>
2020-02-06 13:36:01 +0100

    opdracht 5 af

--- opdracht05/js/script.js
+++ opdracht05/js/script.js
@@ -6,38 +6,44 @@ var roodLichtStaatAan = false;
 var oranjeLichtStaatAan = false;
 var groenLichtStaatAan = false;
 
-window.onload = function(){
+window.onload = function () {
     document.getElementById("light-top").innerHTML = stop;
     document.getElementById("light-middle").innerHTML = maybe;
     document.getElementById("light-bottom").innerHTML = go;
 }
 
-function toggleRoodLicht(){
-    if(roodLichtStaatAan == false){
+function toggleRoodLicht() {
+    if (roodLichtStaatAan == false) {
         document.getElementById("light-top").style.backgroundColor = "red";
         roodLichtStaatAan = true;
     }
-    else{
+    else {
         document.getElementById("light-top").style.backgroundColor = "gray";
         roodLichtStaatAan = false;
     }
 }
 
-function toggleOranjeLicht(){
-    if(document.getElementById("light-middle").style.backgroundColor == "gray"){
-        document.getElementById("light-middle").style.backgroundColor = "orange"
+function toggleOranjeLicht() {
+    if (oranjeLichtStaatAan == false) {
+        document.getElementById("light-middle").style.backgroundColor = "orange";
+        oranjeLichtStaatAan = false
     }
-    else{
+
+    else {
         document.getElementById("light-middle").style.backgroundColor = "gray";
+        oranjeLichtStaatAan = false;
+    }
+
+
+    function toggleGroenLicht() {
+        if (groenLichtStaatAan == false) {
+            document.getElementById("left-light-bottom").style.backgroundColor = "green";
+            groenLichtStaatAan = true;
+        }
+        else {
+            document.getElementById("left-light-bottom").style.backgroundColor = "gray";
+            groenLichtStaatAan = false;
+        }
     }
 }
 
-function toggleGroenLicht(){
-    if(document.getElementById("light-bottom").style.backgroundColor == "gray"){
-        document.getElementById("light-bottom").style.backgroundColor = "green";
-    }
-    else{
-        document.getElementById("light-bottom").style.backgroundColor = "gray";
-    }
-    
-}
